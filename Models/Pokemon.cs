7c0a944269e8011356f95627a83bfd6f50b12bba
Abel Cristofer <123777413+Cristofer3097@users.noreply.github.com>
2025-07-24 15:07:35 -0600

    Se modifico el model para los sprites en gifs

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -19,15 +19,17 @@ namespace PokeApp.Models
         public string? NameFilter { get; set; }
         public string? SpeciesFilter { get; set; }
     }
+    //Obtencion de datos pokemon
     public class Pokemon
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public PokemonSprites Sprites { get; set; } = new PokemonSprites(); // Cambiado a PokemonSprites
+        public PokemonSprites Sprites { get; set; } = new PokemonSprites(); 
         public List<PokemonType> Types { get; set; } = new List<PokemonType>();
 
         public string? Description { get; set; }
-
+        public int Height { get; set; } 
+        public int Weight { get; set; }
     }
     public class PokemonSprites
     {
@@ -37,8 +39,31 @@ namespace PokeApp.Models
         // Añadimos la propiedad 'Other' que contiene las imágenes de alta calidad
         [JsonProperty("other")]
         public OtherSprites? Other { get; set; }
+
+        [JsonProperty("versions")]
+        public VersionsSprites? Versions { get; set; }
+    }
+    public class VersionsSprites
+    {
+        [JsonProperty("generation-v")]
+        public GenerationV? GenerationV { get; set; }
+    }
+    public class GenerationV
+    {
+        [JsonProperty("black-white")]
+        public BlackWhiteSprites? BlackWhite { get; set; }
+    }
+    public class BlackWhiteSprites
+    {
+        [JsonProperty("animated")]
+        public AnimatedSprites? Animated { get; set; }
     }
 
+    public class AnimatedSprites
+    {
+        [JsonProperty("front_default")]
+        public string? FrontDefault { get; set; }
+    }
     public class OtherSprites
     {
         [JsonProperty("official-artwork")]
