e314cd0a88a2a0dd1446d73ae72cd1df7a29e829
Abel Cristofer <123777413+Cristofer3097@users.noreply.github.com>
2025-08-01 16:12:25 -0600

    comando para publicarlo
    dotnet publish -c Release -o out

--- obj/Debug/net9.0/PokeApp.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/PokeApp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47ffd812ec90ad42a45bad0840e968bf6d0bbcb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07c9b64d1a33a5f21aff8d44243cc2bccf491f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
