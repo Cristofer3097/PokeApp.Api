43be570789dead6e1cfbb24025a38af8e29647d4
Abel Cristofer <123777413+Cristofer3097@users.noreply.github.com>
2025-08-06 14:55:35 -0600

    actualizacion

--- obj/Debug/net9.0/PokeApp.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/PokeApp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9b312c9ce35b89246e94cb4da64c942c688dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06b299304c306c199c693ebb41b1e2c8ea182d9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
