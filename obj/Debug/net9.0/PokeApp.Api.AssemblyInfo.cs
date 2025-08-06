2a9b312c9ce35b89246e94cb4da64c942c688dfa
Abel Cristofer <123777413+Cristofer3097@users.noreply.github.com>
2025-08-06 13:57:00 -0600

    carga mas efectiva y llama a varias evoluciones al mismo tiempo

--- obj/Debug/net9.0/PokeApp.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/PokeApp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5932bd381b33048197b12c0fcadc2ea7a71d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03862d6e703d04a39bd45f684e1fff33bb028ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
